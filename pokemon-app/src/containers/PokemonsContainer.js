dcceddfe2602034c04e390cc77dfd193830da37f
jjyoon7 <jay.yoon7@gmail.com>
2020-04-06 20:58:07 +0200

    pass 2 arguments to the useQuerry hook to fetch specific data from graphql

--- pokemon-app/src/containers/PokemonsContainer.js
+++ pokemon-app/src/containers/PokemonsContainer.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {useQuerry} from '@apollo/react-hooks'
+import { useQuerry } from '@apollo/react-hooks'
 import { Pokemon } from '../components/Pokemon'
 
 export default function PokemonsContainer() {
+    const { data: { pokemons = [] } = {} } = useQuerry(GET_POKEMONS, {
+        variables: { first: 9 },
+    })
+
     return (
         <p>pokemon is here</p>
     )
