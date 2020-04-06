59ffcdda38a1b626ad6182f476288599f0001b28
jjyoon7 <jay.yoon7@gmail.com>
2020-04-06 19:56:24 +0200

    set up ApolloClient

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ApolloClinet from 'apollo-boost'
 
 
 function App() {
+  const clinet = new ApolloClinet({
+    uri: 'https://graphql-pokemon.now.sh/'
+  })
+
   return (
     <div className="App">
       test
