04f24d68231a1933bb85b9844f0380b4f71ed7c2
jjyoon7 <jay.yoon7@gmail.com>
2020-04-07 18:51:49 +0200

    do not forget to give a key when you map over array

--- pokemon-app/src/components/Pokemon.js
+++ pokemon-app/src/components/Pokemon.js
@@ -14,9 +14,9 @@ export function Pokemon({pokemon}) {
                 <img src={pokemon.image} alt={pokemon.name}/>
             </div>
             <div className="pokemon__attacks">
-                {/* {pokemon.attacks.map(attack => {
-                    
-                })} */}
+                {pokemon.attacks.special.slice(0, 3).map(attack => (
+                    <span key={`${attack.name}=${attack.damage}`}>{attack.name}</span>
+                ))}
             </div>
         </div>
     )
