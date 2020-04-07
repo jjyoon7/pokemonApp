5a1e7067b5a7a591971d2fe709cb72bbbea690ce
jjyoon7 <jay.yoon7@gmail.com>
2020-04-07 19:08:29 +0200

    add css

--- pokemon-app/src/containers/PokemonsContainer.js
+++ pokemon-app/src/containers/PokemonsContainer.js
@@ -5,7 +5,7 @@ import { GET_POKEMONS } from '../graphql/get-pokemons'
 
 export default function PokemonsContainer() {
     const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-        variables: { first: 9 },
+        variables: { first: 15 },
     })
 
     return (
